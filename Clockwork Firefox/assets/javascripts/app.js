a9f4ad2ec7070a187795c63516c382f9c26cfab1
its <itsgoingd@luzer.sk>
2018-05-30 13:15:14 +0200

    Updated to Angularjs 1.7.

--- Clockwork Firefox/assets/javascripts/app.js
+++ Clockwork Firefox/assets/javascripts/app.js
@@ -1,3 +1,4 @@
 let Clockwork = angular.module('Clockwork', [])
+	.config($sceProvider => $sceProvider.enabled(false))
 	.factory('requests', () => new Requests)
 	.factory('updateNotification', () => new UpdateNotification)
