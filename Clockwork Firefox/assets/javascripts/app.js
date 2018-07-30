6c47079277883415d2b5c79459a3c465533a0592
its <itsgoingd@luzer.sk>
2018-07-31 01:07:41 +0200

    Synced with latest Chrome version.

--- Clockwork Firefox/assets/javascripts/app.js
+++ Clockwork Firefox/assets/javascripts/app.js
@@ -1,4 +1,15 @@
-let Clockwork = angular.module('Clockwork', [])
+let Clockwork = angular.module('Clockwork', [ 'chart.js', 'ngclipboard' ])
+	.config([ '$compileProvider', ($compileProvider) => {
+		$compileProvider.debugInfoEnabled(false)
+		$compileProvider.commentDirectivesEnabled(false)
+	} ])
 	.config($sceProvider => $sceProvider.enabled(false))
+	.factory('filter', [ '$timeout', ($timeout) => {
+		return {
+			create (tags, mapValue) { return new Filter(tags, mapValue, $timeout) }
+		}
+	} ])
+	.factory('profiler', [ 'requests', requests => new Profiler(requests) ])
 	.factory('requests', () => new Requests)
 	.factory('updateNotification', () => new UpdateNotification)
+	.filter('profilerMetric', [ 'profiler', profiler => profiler.metricFilter() ])
